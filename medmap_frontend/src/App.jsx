f0f64c164ee4cee1bc54f17baebe3857354859c3
Ram Govind V <ramgovindvi.in@gmail.com>
2023-05-14 12:56:28 +0530

    Update App.jsx

--- medmap_frontend/src/App.jsx
+++ medmap_frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';  
+<<<<<<< Updated upstream
 import PharmacyProfile from './components/PharmacyProfile';
 import Navbar from "./components/Navbar";
 import Inventory from './components/Inventory.jsx';
@@ -7,6 +8,18 @@ import Orders from './components/Orders.jsx';
 import Home from "./components/Home.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Feedback from "./components/Feedback";
+=======
+import PharmacyProfile from './pages/PharmacyProfile';
+import Inventory from './pages/Inventory.jsx';
+import About from './pages/AboutUs.jsx';
+import Orders from './pages/Orders.jsx';
+import Home from "./pages/Home.jsx";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Feedback from "./pages/Feedback";
+import Login from './pages/Login';
+// import Register from './pages/register';
+
+>>>>>>> Stashed changes
 function App() {
 
   return (
@@ -14,6 +27,11 @@ function App() {
       <Navbar/>
       <BrowserRouter>
         <Routes>
+<<<<<<< Updated upstream
+=======
+          <Route path="login" element={<Login />} />
+          {/* <Route path="register" element={<Register />} /> */}
+>>>>>>> Stashed changes
             <Route path="/" element={<Home />} />
             <Route path="/aboutus" element={<About />} />
             <Route path="/orders" element={<Orders />} />
